255737f902e9508f5d1633f1f9179eb6f13a62f8
Jeroen Leenarts <jeroen.leenarts@gmail.com>
2023-02-22 09:22:56 +0100

    Send new items to Algolia.

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,9 @@
 const crypto = require("crypto");
 const bcrypt = require("bcrypt");
+const algoliasearch = require('algoliasearch');
+
 require("dotenv").config();
 
-const webhook_token = process.env.WEBHOOK_TOKEN;
 const {
     loginHandler,
     registerUser,
@@ -53,6 +54,11 @@ const users = async (req, res) => {
 };
 
 const feedWebhook = async (req, res) => {
+    const webhook_token = process.env.WEBHOOK_TOKEN;
+    const algolia_app_id = process.env.ALGOLIA_APP_ID;
+    const algolia_api_key = process.env.ALGOLIA_API_KEY;
+    const algolia_index_name = process.env.ALGOLIA_INDEX_NAME;
+
     if (req.query.webhook_token != webhook_token) {
         res.status(403).send('Forbidden');
         return;
@@ -62,8 +68,24 @@ const feedWebhook = async (req, res) => {
         return;
     }
     try {
+        const client = algoliasearch(algolia_app_id, algolia_api_key);
+        const index = client.initIndex(algolia_index_name);
         console.log(JSON.stringify(req.body));
-        res.status(200).send('OK');
+        req.body.forEach(element => {
+            const newItems = element.new;
+            const deletedItems = element.deleted;
+            newItems.forEach(item => {
+                item.objectID = item.id;
+                // delete item.actor["data"];
+                item.time = new Date(item.time).getTime();
+                item.actor.created_at = new Date(item.actor.created_at).getTime();
+                item.actor.updated_at = new Date(item.actor.updated_at).getTime();
+                // console.log(JSON.stringify(item));
+            });
+
+            index.saveObjects(newItems, { autoGenerateObjectIDIfNotExist: false });
+        });
+        res.status(200).json(req.body)
     } catch (error) {
         console.log(error);
         res.status(500).json({ message: error.message });
